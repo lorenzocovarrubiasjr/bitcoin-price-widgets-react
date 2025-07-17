dbb0f94c82f3ff5d5ee9627226cfbcc16e968ee2
Witeout <lorenzocovarrubiasjr@gmail.com>
2025-07-16 23:51:25 -0500

    added storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
+import './components/BitcoinWidget/BitcoinWidget.css'
 export { BitcoinWidget } from './components/BitcoinWidget/BitcoinWidget';
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      include: ['src/**/*'],
-      outDir: './dist',
-    })
-  ],
+  plugins: [react(), dts({
+    include: ['src/**/*'],
+    outDir: './dist'
+  })],
   build: {
     lib: {
       entry: './src/index.tsx',
-      name: 'ReactBitcoin',
-      fileName: (format) => `bitcoin-price-widgets-react.${format}.js`,
+      name: 'BitcoinPriceWidgetsReact',
+      fileName: format => `bitcoin-price-widgets-react.${format}.js`,
       formats: ['es', 'umd']
     },
     rollupOptions: {
@@ -28,5 +32,28 @@ export default defineConfig({
       }
     },
     sourcemap: true
+  },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
   }
-})
+});
