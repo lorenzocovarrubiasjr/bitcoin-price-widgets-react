06c18d8faa1774ba0c8694cc18ef0c637b310884
Witeout <lorenzocovarrubiasjr@gmail.com>
2025-07-25 13:18:07 -0500

    added BitcoinChip component

---
+++ src/components/BitcoinChip/BitcoinChip.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import BTCShortLogo from '../../assets/bitcoin-short-logo.png'
+import './BitcoinChip.css'
+
+const Platform = {
+    'coingecko': 'CoinGecko API'
+}
+
+const BitcoinChip = () => {
+    const [currentPrice, setCurrentPrice] = useState<string>();
+    const [lastUpdated, setLastUpdated] = useState<Date>();
+
+    const fetchBTCPrice = async () => {
+        const resp = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        if (!resp.ok) throw new Error("Error fetchinb BTC Price");
+
+        const data = await resp.json();
+        console.log({ data })
+        setCurrentPrice(new Intl.NumberFormat().format(data.bitcoin.usd))
+        setLastUpdated(new Date());
+    }
+
+    useEffect(() => {
+        fetchBTCPrice();
+        const refreshPrice = setInterval(() => fetchBTCPrice(), 30000);
+
+        return () => clearInterval(refreshPrice)
+    }, [])
+
+    return (
+        <div className="container">
+            <div className='logo-sm'>
+                <img src={BTCShortLogo} />
+            </div>
+            <div className="currency-container-sm">
+                <div className="info-column-sm">
+                    <div className="symbol-sm">$</div>
+                    <div className="currency-sm">USD</div>
+                </div>
+                <div className="current-price-sm">
+                    {currentPrice}
+                </div>
+            </div>
+            {/* <div className="bottom-section">
+
+                <div> {lastUpdated && <p>{Platform.coingecko} last update: {lastUpdated.toLocaleString()}</p>}</div>
+            </div> */}
+            <div className="blinking-dot-md"></div>
+        </div>
+    )
+}
+
+export { BitcoinChip };
